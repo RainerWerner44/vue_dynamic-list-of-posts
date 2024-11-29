c0728986411c45d79b99d2cce74cd0dd5b15e618
Vadym Babisov <vadim.babisov@gmail.com>
2024-11-29 13:17:31 +0200

    added solution

--- vue_dynamic_list_of_posts/src/App.vue
+++ vue_dynamic_list_of_posts/src/App.vue
@@ -1,85 +1,148 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import MainHeader from './components/MainHeader.vue'
+import PostsList from './components/PostsList.vue'
+import UserLogin from './components/UserLogin.vue'
+import AddPostForm from './components/AddPostForm.vue'
+import PostBody from './components/PostBody.vue'
+import type { Post } from './types/Post'
+
+const isAuth = ref(false)
+const showLogin = ref(false)
+const showAddPostForm = ref(false)
+const userPosts = ref(JSON.parse(localStorage.getItem('posts') || '[]'))
+const selectedPost = ref<{ id: number; title: string; body: string } | null>(
+  null,
+)
+const isPostEditing = ref(false)
+
+onMounted(() => {
+  const isUserAuth = localStorage.getItem('email') || ''
+
+  if (isUserAuth !== '') {
+    showLogin.value = false
+  }
+})
+
+const handleAuthStatus = (authStatus: boolean) => {
+  isAuth.value = authStatus
+  showLogin.value = !isAuth.value
+}
+
+const openAddPostForm = () => {
+  showAddPostForm.value = true
+  selectedPost.value = null
+}
+
+const closeAddPostForm = () => {
+  showAddPostForm.value = false
+}
+const addNewPost = (post: { id: number; title: string; body: string }) => {
+  userPosts.value = [...userPosts.value, post]
+
+  selectedPost.value = post
+  showAddPostForm.value = false
+
+  localStorage.setItem('posts', JSON.stringify(userPosts.value))
+}
+
+const togglePost = (post: { id: number; title: string; body: string }) => {
+  if (selectedPost.value === null || selectedPost.value?.id !== post.id) {
+    selectedPost.value = post
+  } else {
+    selectedPost.value = null
+  }
+
+  showAddPostForm.value = false;
+}
+
+const deletePost = (id: number) => {
+  userPosts.value = userPosts.value.filter((post: Post) => post.id !== id)
+  localStorage.setItem('posts', JSON.stringify(userPosts.value))
+  selectedPost.value = null
+}
+
+const updatePost = (updatedPost: {
+  id: number
+  title: string
+  body: string
+}) => {
+  const index = userPosts.value.findIndex(
+    (post: Post) => post.id === updatedPost.id,
+  )
+  if (index !== -1) {
+    userPosts.value[index] = updatedPost
+    localStorage.setItem('posts', JSON.stringify(userPosts.value))
+    selectedPost.value = updatedPost
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <UserLogin v-if="showLogin" @authStatus="handleAuthStatus" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div v-else>
+    <MainHeader @authStatus="handleAuthStatus" />
+    <div class="is-flex full-width flex-gap">
+      <PostsList
+        class="flex-item"
+        @openAddPostForm="openAddPostForm"
+        :posts="userPosts"
+        :showAddPostForm="showAddPostForm"
+        :selectedPost="selectedPost"
+        @togglePost="togglePost"
+        :isPostEditing="isPostEditing"
+      />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <transition name="slide">
+        <AddPostForm
+          class="flex-item"
+          v-if="showAddPostForm && selectedPost === null"
+          @addPost="addNewPost"
+          @closePostForm="closeAddPostForm"
+        />
+      </transition>
+
+      <transition name="slide">
+        <PostBody
+          :post="selectedPost"
+          v-if="selectedPost !== null"
+          class="flex-item"
+          @deletePost="deletePost"
+          @updatePost="updatePost"
+          v-model:isPostEditing="isPostEditing"
+        />
+      </transition>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+.full-width {
+  --container-width: 1200px;
+  max-width: var(--container-width);
+  margin: 0 auto;
+  margin-top: 40px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.flex-item {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.flex-gap {
+  gap: 1.5rem;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.5s ease;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.slide-enter-from, .slide-leave-from {
+  transform: translateX(50%);
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.slide-enter-to, .slide-leave-from {
+  transform: translateX(0);
 }
 </style>
--- vue_dynamic_list_of_posts/src/main.ts
+++ vue_dynamic_list_of_posts/src/main.ts
@@ -1,8 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 const app = createApp(App)
 
